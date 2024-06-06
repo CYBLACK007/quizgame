cfc781edb10a9e6b5b208da9053268a9ceb549c9
cyblack <70537305+CYBLACK007@users.noreply.github.com>
2024-06-06 09:47:38 -0300

    Update BancoQuestoes.java

--- src/BancoQuestoes.java
+++ src/BancoQuestoes.java
@@ -54,6 +54,51 @@ questoes.add(new Questao("Qual jogo popular apresenta uma batalha entre equipes 
 
 questoes.add(new Questao("Qual jogo foi lançado com o console Nintendo Switch e se tornou um sucesso imediato?",
                 new String[]{"A) The Legend of Zelda: Breath of the Wild", "B) Mario Kart 8 Deluxe", "C) Splatoon 2", "D) Super Mario Odyssey"}, 0));
+        questoes.add(new Questao("Qual é o nome do mundo fictício onde se passa a série de jogos Final Fantasy VII?",
+                new String[]{"A) Midgar", "B) Gaia", "C) Spira", "D) Ivalice"}, 1));
+
+questoes.add(new Questao("Em que ano foi lançado o primeiro jogo da série Halo?",
+                new String[]{"A) 2000", "B) 2001", "C) 2002", "D) 2003"}, 1));
+
+questoes.add(new Questao("Qual personagem é conhecido por sua frase 'Do a barrel roll!' em Star Fox?",
+                new String[]{"A) Fox McCloud", "B) Falco Lombardi", "C) Peppy Hare", "D) Slippy Toad"}, 2));
+
+questoes.add(new Questao("Qual jogo popular é ambientado em uma escola fictícia chamada Bullworth Academy?",
+                new String[]{"A) Bully", "B) Persona 5", "C) Life is Strange", "D) Danganronpa"}, 0));
+
+questoes.add(new Questao("Qual desses jogos pertence ao gênero Battle Royale?",
+                new String[]{"A) Fortnite", "B) Overwatch", "C) Destiny 2", "D) Warframe"}, 0));
+
+questoes.add(new Questao("Qual é o nome do protagonista da série de jogos God of War?",
+                new String[]{"A) Kratos", "B) Zeus", "C) Ares", "D) Hades"}, 0));
+
+questoes.add(new Questao("Qual desses jogos foi desenvolvido pela Bethesda Game Studios?",
+                new String[]{"A) The Elder Scrolls V: Skyrim", "B) The Witcher 3: Wild Hunt", "C) Dark Souls III", "D) Mass Effect 3"}, 0));
+
+questoes.add(new Questao("Qual é o nome do primeiro jogo da série Assassin's Creed?",
+                new String[]{"A) Assassin's Creed", "B) Assassin's Creed II", "C) Assassin's Creed: Brotherhood", "D) Assassin's Creed: Revelations"}, 0));
+
+questoes.add(new Questao("Em que ano foi lançado o jogo The Last of Us?",
+                new String[]{"A) 2011", "B) 2012", "C) 2013", "D) 2014"}, 2));
+
+questoes.add(new Questao("Qual desses jogos é conhecido pelo seu modo de construção e sobrevivência?",
+                new String[]{"A) Minecraft", "B) Terraria", "C) Rust", "D) Todos os anteriores"}, 3));
+
+questoes.add(new Questao("Qual é o nome do protagonista em Red Dead Redemption 2?",
+                new String[]{"A) John Marston", "B) Arthur Morgan", "C) Dutch van der Linde", "D) Micah Bell"}, 1));
+
+questoes.add(new Questao("Qual desses jogos é uma série de RPGs de ação da FromSoftware?",
+                new String[]{"A) Final Fantasy", "B) Dark Souls", "C) The Witcher", "D) Dragon Age"}, 1));
+
+questoes.add(new Questao("Qual é o nome do jogo de simulação de vida desenvolvido pela Maxis?",
+                new String[]{"A) The Sims", "B) SimCity", "C) Animal Crossing", "D) Stardew Valley"}, 0));
+
+questoes.add(new Questao("Qual desses jogos é conhecido por sua mecânica de parkour?",
+                new String[]{"A) Mirror's Edge", "B) Prince of Persia", "C) Assassin's Creed", "D) Todos os anteriores"}, 3));
+
+questoes.add(new Questao("Qual jogo é famoso por seu personagem que coleta anéis dourados?",
+                new String[]{"A) Mario", "B) Sonic", "C) Mega Man", "D) Kirby"}, 1));
+
 
     }
 
